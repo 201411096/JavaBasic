95d5ff1fc164df61760465818ffe86d98113589b
201411096 <korea5781@naver.com>
2020-03-20 20:35:36 +0900

    .

--- JavaBasic/src/temp/ex.java
+++ JavaBasic/src/temp/ex.java
@@ -6,33 +6,46 @@ public class ex{
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
-		int n, m ;
-		n=scanner.nextInt();
-		char star[][] = new char[n][];
-		
-		for(int i=0; i<star.length; i++)
+		int testCase = scanner.nextInt();
+		scanner.nextLine();
+		String testNum[] = new int [testCase];
+		int testDigit[] = new int [testCase];
+		int answer[] = new int[testCase];
+		for(int i=0; i<testCase; i++) //테스트케이스 수 만큼 n과 십진수 수를 입력받음
 		{
-			if(i%2==0)
-				star[i] = new char[10];
-			else
-				star[i] = new char[2*i];
-			for(int j=0; j<star[i].length; j++)
+			String str = scanner.nextLine();
+			StringTokenizer st = new StringTokenizer(str);
+			testNum[i]=st.nextToken();
+			testDigit[i]=Integer.parseInt(st.nextToken());
+		}
+		for(int i=0; i<testCase; i++)
+		{
+			answer[i]=1;
+			int testNum_digitCnt =0; //n진법에서의 자릿수 변수
+			int temp = testNum[i]; //n진법에서의 자릿수를 구하는데 사용할 변수
+			while(temp!=0) //n진법에서의 자릿수를 구함
 			{
-				star[i][j]='*';
+				temp/=testDigit[i];
+				testNum_digitCnt++;
+			}
+			int temp2 = testNum[i];
+			int testNum_in_testDigit[] = new int[testNum_digitCnt]; //n진법에서의 자릿수만큼 배열 지정
+			for(int j=0; j<testNum_digitCnt; j++) //n진법으로 변환
+			{
+				testNum_in_testDigit[j] = temp2%testDigit[i];
+				temp2/=testDigit[i];
+			}
+			for(int j=0; j<testNum_digitCnt/2; j++)
+			{
+				if(testNum_in_testDigit[j]!=testNum_in_testDigit[testNum_digitCnt-j-1])
+				{
+					answer[i]=0;
+				}
+					
 			}
 		}
-		for(int i=0; i<star.length; i++)
-		{
-			for(int j=0; j<star[i].length; j++)
-			{
-				System.out.print(star[i][j]);
-			}
-			System.out.println();
-		}
-		
-		
-		
-		
+		for(int i=0; i<testCase; i++)
+			System.out.println(answer[i]);
 	}
 }
 
