e272371b309668aaedfbd7ea70719b8dfe267dd2
Canon <Canon@DESKTOP-PL2F7PK>
2020-03-20 16:32:54 +0900

    commit 2

--- JavaBasic/src/d_array/Ex08_OX.java
+++ JavaBasic/src/d_array/Ex08_OX.java
@@ -1,5 +1,34 @@
 package d_array;
 
-public class Ex08_OX {
+import java.util.Scanner;
 
+public class Ex08_OX {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int testCase=scanner.nextInt();
+		scanner.nextLine();
+		int answer[] = new int[testCase];
+		int plus =1;
+		for(int n=0; n<testCase; n++)
+		{
+			String str = scanner.nextLine();
+			plus=1;
+			for(int i=0; i<str.length(); i++) // 문자열에 속한 문자의 개수만큼
+			{
+				if(str.charAt(i)=='O')
+				{
+					answer[n]+=plus++;
+				}else
+				{
+					plus=1;
+				}
+			}
+		}
+		
+		for(int i=0; i<testCase; i++)
+		{
+			System.out.println(answer[i]);
+		}
+	}
 }
+
--- JavaBasic/src/d_array/Ex08_avg.java
+++ JavaBasic/src/d_array/Ex08_avg.java
@@ -1,5 +1,40 @@
 package d_array;
 
-public class Ex08_avg {
+import java.util.Scanner;
 
+public class Ex08_avg {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int testCase = scanner.nextInt();
+		scanner.nextLine();
+		double answer[] = new double[testCase];
+		for(int t=0; t<testCase; t++)
+		{
+			int stdNum= scanner.nextInt();
+			int score[] = new int [stdNum];
+			int sum=0;
+			int cnt=0;
+			for(int i=0; i<score.length; i++) //학생 수만큼 점수 입력 받기
+			{
+				score[i]=scanner.nextInt();
+				sum+=score[i];
+			}
+			for(int i=0; i<score.length; i++)
+			{
+				if(score[i]>(sum/stdNum))
+					cnt++;
+			}
+			answer[t]= (double)cnt/stdNum*100;
+			
+		}
+		
+		for(int i=0; i<answer.length; i++)
+		{
+//			System.out.println(Math.round(answer[i]*1000)/1000.0 + "%"); //소수점 3자리까지 표현 1
+			System.out.printf("%.3f", answer[i]);
+			System.out.print("%");
+			System.out.println();
+		}
+	}
 }
+
