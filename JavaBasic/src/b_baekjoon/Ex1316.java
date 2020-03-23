6999666c9e19105e023ce0c34e91538836c3fae8
Canon <Canon@DESKTOP-PL2F7PK>
2020-03-23 10:00:10 +0900

    200323_01

---
+++ JavaBasic/src/b_baekjoon/Ex1316.java
@@ -0,0 +1,41 @@
+package b_baekjoon;
+
+import java.util.Scanner;
+
+public class Ex1316 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int testCase = scanner.nextInt();
+		scanner.nextLine();
+		String stringArray[] = new String[testCase];
+		int answer=testCase;
+		for(int i=0; i<testCase; i++)
+		{
+			stringArray[i]=scanner.nextLine();
+		}
+		
+		for(int i=0; i<testCase; i++)
+		{
+			int alphabet_cnt[] = new int[26];
+			char pre_alphabet = stringArray[i].charAt(0);
+			alphabet_cnt[ (int)(pre_alphabet-'a') ] = 1;
+			for(int j=1; j<stringArray[i].length(); j++)
+			{
+				if(stringArray[i].charAt(j)!=pre_alphabet && alphabet_cnt[(int)(stringArray[i].charAt(j)-'a')]==1)
+				{
+					answer--;
+					break;
+				}
+				alphabet_cnt[ (int)(stringArray[i].charAt(j)-'a') ] = 1;
+				pre_alphabet=stringArray[i].charAt(j);
+			}
+		}
+		
+		
+		
+		System.out.println(answer);
+	}
+
+}
+
---
+++ JavaBasic/src/e_method/Ex01_흐름2.java
@@ -0,0 +1,31 @@
+package e_method;
+
+public class Ex01_흐름2 {
+
+	void method(){
+		System.out.println("method 실행");
+	}
+	
+	public static void main(String[] args) {
+		Ex01_흐름2 ex = new Ex01_흐름2();
+		System.out.println("메인 함수 시작");
+		ex.method();
+		System.out.println("메인 함수 끝");
+	}
+	
+
+}
+/*
+ * 명명규칙
+ * 1. 문자+숫자+_+$ 조합
+ * 2. 길이제한 없음
+ * 3. 첫글자에 숫자만 아니면 됨
+ * 4. 대소문자 구별
+ * 5. 예약어는 사용할 수 없음
+ * 권장사항
+ * 	- 클래스명의 첫글자는 대문자시작
+ * 	- 변수와 메소드명의 첫글자는 소문자로 시작
+ * 	- 라벨명은 전부 대문자로
+ * 	- 패키지명은 전부 소문자로
+ */
+
