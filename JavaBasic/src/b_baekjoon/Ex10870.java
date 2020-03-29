f7956a8850ed09f782f0ef28637d2e5e85ff32f9
201411096 <korea5781@gmail.com>
2020-03-30 04:13:17 +0900

    200330_02

---
+++ JavaBasic/src/b_baekjoon/Ex10870.java
@@ -0,0 +1,22 @@
+package b_baekjoon;
+
+import java.util.Scanner;
+
+public class Ex10870 {
+	static int func(int n) {
+		if(n==0)
+			return 0;
+		else if(n<=2)
+			return 1;
+		else
+			return func(n-1)+func(n-2);
+			
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();		
+		int answer = func(n);
+		System.out.println(answer);
+	}
+}
