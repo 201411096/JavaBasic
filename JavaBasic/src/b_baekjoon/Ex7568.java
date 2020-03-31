f4e03fafd5a5008a72da8b2544a51798d17d25bf
201411096 <korea5781@gmail.com>
2020-03-31 17:25:21 +0900

    200331_03

---
+++ JavaBasic/src/b_baekjoon/Ex7568.java
@@ -0,0 +1,33 @@
+package b_baekjoon;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Ex7568  {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		scanner.nextLine();
+		int kg [] = new int[num];
+		int height [] = new int [num];
+		int rank [] = new int [num];
+		for(int i=0; i<num; i++)  // 입력
+		{
+			StringTokenizer st = new StringTokenizer(scanner.nextLine());
+			kg[i]= Integer.parseInt(st.nextToken());
+			height[i]= Integer.parseInt(st.nextToken());
+			rank[i]=1;
+		}
+		for(int i=0; i<num; i++)
+		{
+			for(int j=0; j<num; j++)
+			{
+				if(kg[i]<kg[j] &&height[i]<height[j] && i!=j) // 등호 실수해서 계속 막혔었음
+					rank[i]++;
+			}
+		}
+		for(int i=0; i<num; i++)
+			System.out.print(rank[i] + " ");
+	}
+}
