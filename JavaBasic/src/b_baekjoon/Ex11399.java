6927f0bcb40148a1d4f401cdb9f328690ee92a95
201411096 <korea5781@gmail.com>
2020-04-08 16:08:19 +0900

    200408_07

---
+++ JavaBasic/src/b_baekjoon/Ex11399.java
@@ -0,0 +1,36 @@
+package b_baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex11399{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int inputNumber = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int inputArray [] = new int[inputNumber];
+		int waitingTimeArray [] = new int[inputNumber];
+		int answer=0;
+		for(int i=0; i<inputNumber; i++)
+			inputArray[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(inputArray);
+		
+		for(int i=0; i<inputArray.length; i++)
+		{
+			answer+=inputArray[i]*(inputArray.length-i);
+		}
+
+		bw.write(Integer.toString(answer));
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+}
