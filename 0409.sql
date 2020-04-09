4c44db7d02ac36c6d92f1c83b9d3c9ee134f6114
201411096 <korea5781@gmail.com>
2020-04-09 12:43:20 +0900

    200409_03

--- 0409.sql
+++ 0409.sql
@@ -116,6 +116,7 @@ FROM emp e, dept d
 WHERE e.deptno=d.deptno
 GROUP BY d.dname;   -- d.deptno으로 그룹을 묶을 경우 d.name이 출력이 안됨
                     -- d.name출력을 하고 싶을 경우 d.name으로 묶어줘야 출력이 가능
+--*********************** group by를 했을 경우 select문에 사용할 수 있는 것은 group by에 사용한 컬럼명과 group함수들
 --4. 부서별 부서명과 급여 합계를 출력 (ANSI)
 SELECT d.dname as dname, sum(e.sal) as sal
 FROM emp e INNER JOIN dept d
@@ -124,11 +125,83 @@ GROUP BY d.dname;
 -- 5. 업무가 ‘MANAGER’인 사원의 정보를 이름, 업무, 부서명, 근무지를 출력 (예전 방식)
 SELECT e.ename as ename, e.job as job, d.dname as dname, d.loc as loc
 FROM emp e, dept d
-WHERE e.deptno=d.deptno AND job='MANAGER';
+WHERE e.deptno=d.deptno AND e.job='MANAGER';
 -- 5. 업무가 ‘MANAGER’인 사원의 정보를 이름, 업무, 부서명, 근무지를 출력 (ANSI)
 SELECT e.ename as ename, e.job as job, d.dname as dname, d.loc as loc
 FROM emp e INNER JOIN dept d
 ON e.deptno=d.deptno
 WHERE job='MANAGER';
+----------------------------------------------------------------------------------------------------------------------------
+-------------------------외부 조인------------------------------------------------------
+--예시1 <예전 방식>
+SELECT e.ename AS ename, e.deptno AS deptno, d.dname AS dname
+FROM emp e, dept d
+WHERE e.deptno = d.deptno(+);
+--예시1 <ANSI>
+SELECT e.ename AS ename, e.deptno AS deptno, d.dname AS dname
+FROM emp e LEFT OUTER JOIN dept d -- LEFT, RIGHT는 (+)를 붙이던 곳의 반대쪽으로 붙여야됨
+ON e.deptno = d.deptno;
 
+--예시2 <예전 방식>
+SELECT e.ename AS ename, e.deptno AS deptno, d.dname AS dname
+FROM emp e, dept d
+WHERE e.deptno(+) = d.deptno;
+--예시2 <ANSI>
+SELECT e.ename AS ename, e.deptno AS deptno, d.dname AS dname
+FROM emp e RIGHT OUTER JOIN dept d -- LEFT, RIGHT는 (+)를 붙이던 곳의 반대쪽으로 붙여야됨
+ON e.deptno = d.deptno;
 
+--예시3 <예전 방식 - 오류가 났었던 부분>
+SELECT e.ename AS ename, e.deptno AS deptno, d.dname AS dname
+FROM emp e, dept d
+WHERE e.deptno(+) = d.deptno(+);
+--예시3 <ANSI 새로 구현된 부분>
+SELECT e.ename AS ename, e.deptno AS deptno, d.dname AS dname
+FROM emp e FULL OUTER JOIN dept d -- LEFT, RIGHT는 (+)를 붙이던 곳의 반대쪽으로 붙여야됨
+ON e.deptno = d.deptno;
+
+----------------------------------------------------------------------------------------------------------
+--셀프 조인
+--사원번호,사원명, 매니저번호, 매니저이름
+SELECT e1.empno AS empno, e1.ename AS ename, e1.mgr AS mgr, e2.ename AS mgrname
+FROM emp e1, emp e2 -- e1 : 사원을 찾는 테이블, e2 : 매니저를 찾는 테이블
+WHERE e1.mgr=e2.empno; -- 가장 직급이 높은 president와 상사가 없는 홍길동 데이터가 나오지 않음
+
+SELECT e1.empno AS empno, e1.ename AS ename, e1.mgr AS mgr, e2.ename AS mgrname
+FROM emp e1, emp e2 -- e1 : 사원을 찾는 테이블, e2 : 매니저를 찾는 테이블
+WHERE e1.mgr=e2.empno(+); -- 가장 직급이 높은 president와 상사가 없는 홍길동 데이터가 나오게 하기 위해서 OUTER JOIN 사용
+
+SELECT e1.empno AS empno, e1.ename AS ename, e1.mgr AS mgr, e2.ename AS mgrname
+FROM emp e1 LEFT OUTER JOIN emp e2 -- e1 : 사원을 찾는 테이블, e2 : 매니저를 찾는 테이블
+ON e1.mgr=e2.empno; -- 가장 직급이 높은 president와 상사가 없는 홍길동 데이터가 나오게 하기 위해서 OUTER JOIN 사용
+----------------------------------------------------------------------------------------------------------
+--1. 관리자보다 먼저 입사한 사원에 대해 이름, 입사일, 관리자 이름, 관리자 입사일을 출력
+SELECT e1.ename AS ename, e1.hiredate AS hiredate, e2.ename AS mgrename, e2.hiredate AS mgrhiredate
+FROM emp e1, emp e2
+WHERE e1.mgr = e2.empno AND e1.hiredate<e2.hiredate;
+--(ANSI)
+SELECT e1.ename AS ename, e1.hiredate AS hiredate, e2.ename AS mgrename, e2.hiredate AS mgrhiredate
+FROM emp e1 INNER JOIN emp e2
+ON e1.mgr = e2.empno
+WHERE e1.hiredate<e2.hiredate;
+--2. 사원명에 ‘L’자가 있는 사원에 대해 이름, 업무, 부서명, 위치를 출력
+SELECT e.ename AS ename, e.job AS job, d.dname, d.loc
+FROM emp e, dept d
+WHERE e.deptno = d.deptno AND regexp_like(e.ename, 'L'); -- LIKE '%L%';
+-- LIKE 사용
+SELECT e.ename AS ename, e.job AS job, d.dname, d.loc
+FROM emp e, dept d
+WHERE e.deptno = d.deptno AND e.ename LIKE '%L%';
+--(ANSI)
+SELECT e.ename AS ename, e.job AS job, d.dname, d.loc
+FROM emp e INNER JOIN dept d
+ON e.deptno = d.deptno
+WHERE regexp_like(e.ename, 'L');
+--3. 아래의 결과를 출력 ( 관리자가 없는 KING을 포함하여 모든 사원을 출력 )
+SELECT e1.empno AS empno, e1.ename AS ename, e2.empno AS mgrno, e2.ename AS mgrname
+FROM emp e1, emp e2
+WHERE e1.mgr=e2.empno(+);
+--(ANSI)
+SELECT e1.empno AS empno, e1.ename AS ename, e2.empno AS mgrno, e2.ename AS mgrname
+FROM emp e1 LEFT OUTER JOIN emp e2
+ON e1.mgr=e2.empno;
