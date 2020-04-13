db0a5a059e62edd8d2efc3d03a479fa0e2816a1e
201411096 <korea5781@gmail.com>
2020-04-13 16:02:26 +0900

    200413_11

--- src/basic/TestSelect2.java
+++ src/basic/TestSelect2.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class TestSelect {
+public class TestSelect2 {
 	public static void main(String[] args) {
 		try {
 			//1. 드라이버를 메모리에 로딩
@@ -14,21 +14,14 @@ public class TestSelect {
 			//2. connection 얻어오기
 			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.17:1521:orcl", "scott", "scottscott");//url(ip, port, sid), user password
 			//3. sql문장
-			String sql = "SELECT empno, ename, sal, job FROM emp";
+			String sql = "SELECT count(*) AS cnt FROM EMP"; // 함수나 연산이 있다면 별칭을 주기
 			//4. 전송객체 얻어오기
 			Statement st = con.createStatement();
 			//5. 결과를 받아옴 
 			ResultSet rs = st.executeQuery(sql);
 			//6. 결과물 확인
-			
-			//rs가 스키마를 가리키고 있는 상태 -> rs.next를 "한번 사용해야" 맨 첫번째 레코드를 가리키면서 읽어올 수 있음
-			
-			while(rs.next()) { // 다음 레코드가 있으면 true 없으면 false
-				System.out.print(rs.getInt("EMPNO") + "\t");
-				System.out.print(rs.getString("ENAME") + "\t");
-				System.out.print(rs.getInt("SAL") + "\t");
-				System.out.println(rs.getString("JOB") + "\t");
-			}
+			rs.next();
+			System.out.println("총 "+ rs.getInt("CNT") + "명입니다."); //sql 대소문자를 안 가림
 			//7.닫기
 			rs.close();
 			st.close();
---
+++ src/basic/TestSelect3.java
@@ -0,0 +1,37 @@
+package basic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestSelect3 {
+	public static void main(String[] args) {
+		try {
+			//1. 드라이버를 메모리에 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("성공");
+			//2. connection 얻어오기
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.17:1521:orcl", "hr", "hrhr");//url(ip, port, sid), user password
+			//3. sql문장
+//이전 방식		String sql = "SELECT e.employee_id id, e.first_name||' '||e.last_name ename, d.department_name dname FROM EMPLOYEES e, departments d WHERE salary>=3000 AND e.department_id=d.department_id"; // 함수나 연산이 있다면 별칭을 주기
+//ANSI 방식
+			String sql = "SELECT e.employee_id id, e.first_name||' '||e.last_name ename,d.department_name dname FROM EMPLOYEES e INNER JOIN departments d ON e.department_id=d.department_id WHERE salary>=3000";
+			//4. 전송객체 얻어오기
+			Statement st = con.createStatement();
+			//5. 결과를 받아옴
+			ResultSet rs = st.executeQuery(sql);
+			//6. 결과를 확인
+			while(rs.next()) {
+				System.out.print(rs.getInt("id") +"\t");
+				System.out.print(rs.getString("ename") +"\t");
+				System.out.println(rs.getString("dname") +"\t");
+			}
+			rs.close();
+			st.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
