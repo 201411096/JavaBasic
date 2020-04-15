f68ca12b82001180e5d830ca464bcf996ae52042
201411096 <korea5781@gmail.com>
2020-04-15 21:36:40 +0900

    200415_03

--- JavaBasic/src/t_teamproject_02/CliSocketInServer.java
+++ JavaBasic/src/t_teamproject_02/CliSocketInServer.java
@@ -39,5 +39,4 @@ public class CliSocketInServer extends Thread{
 				
 		}
 	}
-
 }
--- JavaBasic/src/t_teamproject_02/MainClient.java
+++ JavaBasic/src/t_teamproject_02/MainClient.java
@@ -2,6 +2,7 @@ package t_teamproject_02;
 
 public class MainClient {
 	public static void main(String[] args) {
-		MainClientFrame mainClientFrame = new MainClientFrame();
+//		MainClientFrame mainClientFrame = new MainClientFrame();
+		ClientLoginFrame clientLoginFrame = new ClientLoginFrame();
 	}
 }
