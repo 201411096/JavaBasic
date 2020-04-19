2c104bee4af7fe72d74efd9601069b48267b0f42
201411096 <korea5781@gmail.com>
2020-04-19 14:16:19 +0900

    200418_05 제품주문창 기능구현 완료

--- JavaBasic/src/t_teamproject/teamproject_02/temp/dao/ProductManagementDao.java
+++ JavaBasic/src/t_teamproject/teamproject_02/temp/dao/ProductManagementDao.java
@@ -1,5 +1,5 @@
 package t_teamproject.teamproject_02.temp.dao;
 
 public interface ProductManagementDao {
-
+	public int purchaseProductByName(String name, int cnt);
 }
--- JavaBasic/src/t_teamproject/teamproject_02/temp/view/frame/ProductPurchaseFrame.java
+++ JavaBasic/src/t_teamproject/teamproject_02/temp/view/frame/ProductPurchaseFrame.java
@@ -1,10 +1,18 @@
 package t_teamproject.teamproject_02.temp.view.frame;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import t_teamproject.teamproject_02.temp.dao.ProductDao;
 import t_teamproject.teamproject_02.temp.dao.ProductDaoImpl;
@@ -16,6 +24,8 @@ public class ProductPurchaseFrame extends JFrame{
 	ProductDao productDaoModel;
 	String comboBoxMenuArray [];
 	JComboBox comboBoxMenu;
+	JTextField jtextFieldCount;
+	JButton confirmButton;
 	public ProductPurchaseFrame(){
 		connectDB();
 		ArrayList<String> temp = productDaoModel.selectProductNameOrderByName();
@@ -24,20 +34,52 @@ public class ProductPurchaseFrame extends JFrame{
 		eventProc();
 	}
 	public void display() {
-		comboBoxMenu = new JComboBox(comboBoxMenuArray);
+		setLayout(null);
+		
+		comboBoxMenu = new JComboBox(comboBoxMenuArray);		
+		jtextFieldCount = new JTextField();		
+		confirmButton = new JButton("제품 주문");
+
+		comboBoxMenu.setBounds(100, 100, 200, 50);		
+		jtextFieldCount.setBounds(100, 200, 200, 50);		
+		confirmButton.setBounds(100, 300, 200, 50);
+		
 		add(comboBoxMenu);
+		add(jtextFieldCount);
+		add(confirmButton);
 		setTitle("제품 주문 창");
 		setBounds(750, 200, 400, 500);
 		setVisible(true);
 		setBackground(Color.white);
 	}
 	public void eventProc() {
-		
+		confirmButton.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				purchaseProduct();
+			}
+		});
 	}
 	public void connectDB() {
 		try {
 			productDaoModel = new ProductDaoImpl();
 			productManagementmodel = new ProductManagementDaoImpl();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void purchaseProduct() {
+		int cnt = Integer.parseInt(jtextFieldCount.getText());
+		String name = comboBoxMenu.getSelectedItem().toString();
+		
+		try {
+			int result = productManagementmodel.purchaseProductByName(name, cnt);
+			
+			if(result == 0 ) {
+				JOptionPane.showMessageDialog(null, "제품 주문 완료");
+			}else {
+				JOptionPane.showMessageDialog(null, "에러 발생");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
