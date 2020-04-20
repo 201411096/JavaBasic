575bbf403991f91f2da7173f289f8851a3eddd7f
201411096 <korea5781@gmail.com>
2020-04-20 13:13:34 +0900

    200420_09 기능구현완료, void 메소드에도 return 사용 가능(제어권 반환)

--- src/videoshop_0416.sql
+++ src/videoshop_0416.sql
@@ -1,3 +1,6 @@
+--sequence 확인법
+select * from user_sequences;
+/***** CREATE TABLE VIDEO_SHOP_CUSTOMER START *****/
 CREATE TABLE video_shop_customer(
     id number(10),
     name varchar2(10),
@@ -8,11 +11,11 @@ CREATE TABLE video_shop_customer(
     CONSTRAINT video_shop_customer_id_pk primary key(id)
 );
 CREATE SEQUENCE video_shop_customer_id_seq;
-
+/***** CREATE TABLE VIDEO_SHOP_CUSTOMER END *****/
 ALTER TABLE video_shop_customer ADD CONSTRAINT vs_customer_tel_1_unique UNIQUE(tel_1);
 
 
-
+/***** CREATE TABLE VIDEO_SHOP_VIDEO START *****/
 CREATE TABLE video_shop_video(
     vid number(10),
     vgenre varchar2(20),
@@ -22,8 +25,9 @@ CREATE TABLE video_shop_video(
     vdetail varchar2(1024),
     CONSTRAINT video_shop_video_vid_pk primary key(vid)
 );
+/***** CREATE TABLE VIDEO_SHOP_VIDEO END *****/
 SELECT * FROM VIDEO_SHOP_VIDEO WHERE VNAME LIKE '%용용%';
-
+/***** CREATE TABLE VIDEO_SHOP_MANAGEMENT START *****/
 CREATE TABLE video_shop_video_management(
     vmid number(10),
     vid number(10),
@@ -32,8 +36,8 @@ CREATE TABLE video_shop_video_management(
 );
 CREATE SEQUENCE video_shop_video_vid_seq start with 0 minvalue 0;
 CREATE SEQUENCE video_shop_vm_vmid_seq start with 0 minvalue 0;
-
-
+/***** CREATE TABLE VIDEO_SHOP_MANAGEMENT END *****/
+/***** CREATE TABLE VIDEO_SHOP_RENTAL START *****/
 CREATE TABLE video_shop_rental(
     vrid number(10),
     rentaldate date,
@@ -45,7 +49,28 @@ CREATE TABLE video_shop_rental(
     CONSTRAINT video_shop_rental_fk_id FOREIGN KEY(id) REFERENCES video_shop_customer(id)
     
 );
-/*
-drop table video_shop_video;
-drop table video_shop_video_management;
-*/
+CREATE SEQUENCE video_shop_vr_vrid_seq;
+/***** CREATE TABLE VIDEO_SHOP_RENTAL END *****/
+SELECT name FROM video_shop_customer WHERE tel_1=?;
+
+--대여전에 확인하는문장
+SELECT * FROM video_shop_rental WHERE VMID=? AND returndate IS NULL;
+--대여전에 확인해줘야함
+SELECT * FROM video_shop_rental WHERE VMID=47 AND returndate IS NULL;
+INSERT INTO video_shop_rental(VRID, RENTALDATE, VMID, ID) VALUES(video_shop_vr_vrid_seq.NEXTVAL, SYSDATE, 3, 2);
+INSERT INTO video_shop_rental(VRID, RENTALDATE, VMID, ID) VALUES(video_shop_vr_vrid_seq.NEXTVAL, SYSDATE, ?, (SELECT ID FROM video_shop_customer WHERE tel_1=?) );
+SELECT ID FROM video_shop_customer WHERE name=?;
+
+UPDATE video_shop_rental SET returndate=SYSDATE WHERE VMID=? AND returndate IS NULL;
+select to_char(returndate, 'yyyy/mm/dd hh24:mi:ss') from video_shop_rental;
+
+select * from video_shop_rental; --비디오번호 제목 고객명 전화번호 반납예정일(대여날짜에 +3) 반납여부
+
+SELECT R.VMID, V.VNAME, VC.NAME, vc.tel_1, r.rentaldate+3, case 반납여부 when (r.returndate is null) then 'no'
+FROM VIDEO_SHOP_RENTAL R
+INNER JOIN VIDEO_SHOP_VIDEO_MANAGEMENT VM
+ON R.vmid=vm.vmid
+INNER JOIN VIDEO_SHOP_VIDEO V
+ON VM.VID = V.vid
+INNER JOIN video_shop_customer VC
+ON VC.ID = R.ID;
--- src/view/RentView.java
+++ src/view/RentView.java
@@ -4,19 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.AbstractTableModel;
+
+import model.dao.RentModel;
 
 
 
@@ -28,32 +27,61 @@ public class RentView extends JPanel
 	JTextField tfReturnVideoNum;
 	JButton bReturn;
 	
-	JTable tableRecentList;
-	
+	JTable tableRecentList;	
 	RentTableModel rentTM;
 	
-
+	//모델단 변수 선언
+	RentModel model;
 	
 	//==============================================
 	//	 생성자 함수
 	public RentView(){
 		addLayout();	//화면구성
-		eventProc();	//DB연결
-		connectDB();
+		eventProc();	
+		connectDB();	//DB연결
+		
+		selectList();
 	}
-	
+	void selectList() {
+		try {
+			ArrayList data = model.selectList();
+			rentTM.data = data;
+			tableRecentList.setModel(rentTM); // 모델 설정
+			rentTM.fireTableDataChanged();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	// DB 연결
 	void connectDB(){
-		
+		try {
+			model = new RentModel();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
 	// 이벤트 등록
 	public void eventProc(){
-
-		                         
+		BtnHandler btnHandler = new BtnHandler();
+		tfRentTel.addActionListener(btnHandler);
+		bRent.addActionListener(btnHandler);
+		bReturn.addActionListener(btnHandler);
 	}
-	
+	class BtnHandler implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object evt = e.getSource();
+			if(evt==tfRentTel) {
+				selectByTel();
+			}else if(evt==bRent) { //대여처리를 한 후에 목록을 갱신
+				rentVideo();
+				selectList();
+			}else if(evt==bReturn) {//반납처리를 한 후에 목록을 갱신
+				returnVideo();
+				selectList();
+			}
+		}
+	}	
 	/*	객체 생성 및 화면 구성   */
 	void addLayout(){
 		setLayout(new BorderLayout());
@@ -92,35 +120,54 @@ public class RentView extends JPanel
 		center_panel.setLayout(new GridLayout(1, 1));
 		center_panel.add(new JScrollPane(tableRecentList));
 		
-		
 		add(north_panel, BorderLayout.NORTH);
 		add(center_panel, BorderLayout.CENTER);
 	}
 
 	class RentTableModel extends AbstractTableModel { 
-		  
 		ArrayList data = new ArrayList();
 		String [] columnNames = {"비디오번호","제목","고객명","전화번호","반납예정일","반납여부"};
-
 			public int getColumnCount() { 
 		        return columnNames.length; 
 		    } 
-		     
 		    public int getRowCount() { 
 		        return data.size(); 
 		    } 
-
 		    public Object getValueAt(int row, int col) { 
 				ArrayList temp = (ArrayList)data.get( row );
 		        return temp.get( col ); 
-		    }
-		    
+		    }	    
 		    public String getColumnName(int col){
 		    	return columnNames[col];
 		    }
 	}
-
-	
-
-	
+	void selectByTel() {
+		try {
+			String name = model.selectByTel(tfRentTel.getText());
+			tfRentCustName.setText(name);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	void rentVideo() {
+		String tel = tfRentTel.getText();
+		int vnum = Integer.parseInt(tfRentVideoNum.getText());		
+		try {
+			model.rentVideo(tel, vnum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		tfRentTel.setText("");
+		tfRentCustName.setText("");
+		tfRentVideoNum.setText("");
+	}
+	void returnVideo() {
+		int vnum = Integer.parseInt(tfReturnVideoNum.getText());
+		try {
+			model.returnVideo(vnum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		tfReturnVideoNum.setText("");
+	}
 }
